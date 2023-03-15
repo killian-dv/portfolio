2610ca0e0740084b832512d5bc97dde37ce52224
Killian David <killian@air-de-killian.home>
2023-03-15 16:48:27 +0100

    fin about

--- js/script_defer.js
+++ js/script_defer.js
@@ -19,3 +19,39 @@ function  closeSideMenu() {
   const sideMenuToggle = document.querySelector('#active');
   sideMenuToggle.click();
 }
+
+// const menuBtn = document.querySelector('.menu-btn');
+// const closeBtn = document.querySelector('.close');
+// const body = document.querySelector('body');
+
+// menuBtn.addEventListener('click', () => {
+//   body.style.overflow = 'hidden';
+// });
+
+// closeBtn.addEventListener('click', () => {
+//   body.style.overflow = 'auto';
+// });
+
+
+const items = document.querySelectorAll('.card-item');
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return;
+    it.clicked = false;
+  });
+  gsap.to(items, {
+    width: item.clicked ? '15vw' : '8vw',
+    duration: 2,
+    ease: 'elastic(1, .6)'
+  });
+  item.clicked = !item.clicked;
+  gsap.to(item, {
+    width: item.clicked ? '42vw' : '15vw',
+    duration: 2.5,
+    ease: 'elastic(1, .3)'
+  });
+};
+items.forEach((item, i) => {
+  item.clicked = false;
+  item.addEventListener('click', () => expand(item, i));
+});
