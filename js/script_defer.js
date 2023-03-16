ef9aadf04d210416d7c838df83ac563ab527536e
Killian David <killian@air-de-killian.home>
2023-03-16 15:27:47 +0100

    ajout des créations

--- js/script_defer.js
+++ js/script_defer.js
@@ -38,20 +38,29 @@ const expand = (item, i) => {
   items.forEach((it, ind) => {
     if (i === ind) return;
     it.clicked = false;
+    const content = it.querySelector('.card-item-content');
+    content.style.display = 'none';
   });
   gsap.to(items, {
-    width: item.clicked ? '15vw' : '8vw',
+    width: item.clicked ? '20vw' : '10vw',
     duration: 2,
     ease: 'elastic(1, .6)'
   });
   item.clicked = !item.clicked;
   gsap.to(item, {
-    width: item.clicked ? '42vw' : '15vw',
+    width: item.clicked ? '50vw' : '20vw',
     duration: 2.5,
     ease: 'elastic(1, .3)'
   });
+  const content = item.querySelector('.card-item-content');
+  content.style.display = item.clicked ? 'flex' : 'none';
 };
 items.forEach((item, i) => {
   item.clicked = false;
   item.addEventListener('click', () => expand(item, i));
 });
+
+
+
+
+
